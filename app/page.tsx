72a640fed711286f94620c43a753b67e3530a597
jthamma <jthammakit2546@gmail.com>
2024-11-15 00:03:47 +0700

    เพิ่มการ analytic

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Title from './components/Title/Title'
 import Wave from './components/Wave/Wave'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Title />
       <Wave />
-
+      <Analytics/>
     </div>
   );
 }
