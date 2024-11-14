ac3dd30e94c27bff5e9fbe31b6adcbbba5b82871
panachats <143708225+panachats@users.noreply.github.com>
2024-11-14 22:05:44 +0700

    ิbutton

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@ import Title from './components/Title/Title'
 
 export default function Home() {
   return (
-
-    <div className="">
+    <div className="min-h-screen flex flex-col justify-between">
       <Title />
+      <div className="mb-20 flex justify-center w-full">
+        <button className='bg-white rounded-lg p-2 text-2xl hover:bg-gray-100 transition-colors'>
+          สร้างกระทง
+        </button>
+      </div>
     </div>
   );
 }
