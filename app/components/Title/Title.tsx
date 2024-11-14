1943ede5fd12b83646549e75bf5a6ff4e815f43c
jthamma <jthammakit2546@gmail.com>
2024-11-14 22:10:58 +0700

    เพิ่ม 9

--- app/components/Title/Title.tsx
+++ app/components/Title/Title.tsx
@@ -4,15 +4,21 @@ import title from '../../../public/images/title/Title.png'
 
 function Title() {
     return (
-        <div className='flex justify-center p-5'>
+        <div className="flex flex-col items-center p-5 space-y-10">
             <Image
                 src={title}
                 width={800}
                 height={50}
                 alt="title"
-                className='rounded-lg'
+                className="rounded-lg"
                 priority
             />
+            <div className="flex flex-col justify-center items-center space-x-5 w-full">
+                <span>จำนวนธงที่มีการสร้างขึ้น</span>
+                <button className="bg-white rounded-lg p-2 text-2xl hover:bg-gray-100 transition-colors mt-9">
+                    สร้างกระทง
+                </button>
+            </div>
         </div>
     )
 }
