7e9e0c2485fa3136d5dfdc3474323ef5725c9d19
panachats <143708225+panachats@users.noreply.github.com>
2024-11-14 22:56:30 +0700

    แก้ไข wave

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import Title from './components/Title/Title'
+import Wave from './components/Wave/Wave'
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Title />
-      <div className="mb-20 flex justify-center w-full">
-        <button className='bg-white rounded-lg p-2 text-2xl hover:bg-gray-100 transition-colors'>
-          สร้างกระทง
-        </button>
-      </div>
+      <Wave />
+
     </div>
   );
 }
