fde32dc454d42c4aa823ed2e2dc374b356ec425e
jthamma <jthammakit2546@gmail.com>
2024-11-15 08:55:37 +0700

    .env

--- app/config/apisetting.tsx
+++ app/config/apisetting.tsx
@@ -1,2 +1,5 @@
-// src/config/apisetting.ts
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const apiUrl = isProduction
+    ? process.env.NEXT_PUBLIC_API_URL_PRODUCTION
+    : process.env.NEXT_PUBLIC_API_URL;
